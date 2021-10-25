160a60ad74a76d1fa700d84ae643d727b2da83b2
Aman Dalmia <amandalmia18@gmail.com>
2021-10-26 01:43:45 +0530

    NEW: notify particular user ids + accept empty descriptions

--- lambda_function.py
+++ lambda_function.py
@@ -14,7 +14,11 @@ def parse_service_event(event, service="Service"):
             "inline": True,
         },
         {"name": "Alarm", "value": event["AlarmName"], "inline": True},
-        {"name": "Description", "value": event["AlarmDescription"], "inline": True},
+        {
+            "name": "Description",
+            "value": event.get("AlarmDescription", ""),
+            "inline": True,
+        },
         {"name": "Old State", "value": event["OldStateValue"], "inline": True},
         {"name": "Trigger", "value": event["Trigger"]["MetricName"], "inline": True},
         {"name": "Event", "value": event["NewStateReason"], "inline": True},
@@ -24,6 +28,7 @@ def parse_service_event(event, service="Service"):
 def lambda_handler(event, context):
     """Retrieves the SNS event details and makes HTTP call to Discord webhook"""
     webhook_url = os.getenv("WEBHOOK_URL")
+    users_to_notify = os.getenv("USER_IDS_TO_NOTIFY")
     parsed_message = []
     for record in event.get("Records", []):
         # convert SNS message component into JSON
@@ -49,6 +54,11 @@ def lambda_handler(event, context):
                 {"color": 16711680, "fields": parsed_message}  # red to highlight error
             ],
         }
+
+        if users_to_notify:
+            users_to_notify = users_to_notify.split(',')
+            users_to_notify = [f"<@{user_id}>" for user_id in users_to_notify if user_id]
+            discord_data["content"] = "".join(users_to_notify)
 
         headers = {"content-type": "application/json"}
 
